60a490998db1ea7824bcfd1e17a0e743ef79ffec
Aswin TR <aswintr252@gmail.com>
2020-10-29 16:28:22 +0530

    Completed about page, login page, contact us page; removed seperate login page for teacher and student and made it a single login page and added a radio button to recognize the user

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('login/', views.login, name='login'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+
+def login(request):
+    return render(request, 'login.html')
